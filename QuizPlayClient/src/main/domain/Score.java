994e77f74c48946fbee030cc230075ac1569f5fb
MontyWest <monty.p.west@gmail.com>
2014-04-16 13:28:25 +0100

    score inferface change

--- QuizPlayClient/src/main/domain/Score.java
+++ QuizPlayClient/src/main/domain/Score.java
@@ -3,15 +3,8 @@ package domain;
 public interface Score extends Comparable<Score> {
 
 	public int getAmount();
+		
+	public String getName();	
 	
-	public void setAmount(int amount);
-	
-	public String getName();
-	
-	public void setName(String name);
-	
-	public Long getQuizId();
-	
-	public void setQuizId(Long quizId);
-	
+	public Long getQuizId();	
 }
