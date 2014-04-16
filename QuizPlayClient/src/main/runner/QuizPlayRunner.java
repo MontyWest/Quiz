6680157854a41d4d55f21e0c094099fc2b9fee65
MontyWest <monty.p.west@gmail.com>
2014-04-16 16:46:44 +0100

    QuizPlayRunner ready

--- QuizPlayClient/src/main/runner/QuizPlayRunner.java
+++ QuizPlayClient/src/main/runner/QuizPlayRunner.java
@@ -1,11 +1,19 @@
 package runner;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+import domain.PossibleAnswer;
+import domain.Question;
 import domain.Quiz;
 
 public class QuizPlayRunner {
 
 	private Quiz quiz;
 	private int scoreAmount;
+	PrintStream o = System.out;
+	Scanner i = new Scanner(System.in);
 	
 	public QuizPlayRunner(Quiz quiz) {
 		this.quiz = quiz;
@@ -13,7 +21,35 @@ public class QuizPlayRunner {
 	}
 	
 	public void start() {
-		//TODO
+		o.println("\nQuiz " + quiz);
+		TreeSet<Integer> questionNumbers = new TreeSet<Integer>(quiz.getQuestions().keySet());
+		for (Integer i : questionNumbers) {
+			Question question = quiz.getQuestion(i);
+			answerQuestion(question);
+		}
+		o.println("\nPress enter to finish quiz.");
+		i.nextLine();
+	}
+	
+	private void answerQuestion(Question question) {
+		o.println("  Question " + question);
+		TreeSet<Character> answerChars = new TreeSet<Character>(question.getPossibleAnswers().keySet());
+		for (Character c : answerChars) {
+			PossibleAnswer possibleAnswer = question.getPossibleAnswer(c);
+			o.println("    " + possibleAnswer);
+		}
+		PossibleAnswer possibleAnswer = null;
+		while (possibleAnswer == null) {
+			o.print("  Answer (enter character): ");
+			char choice = i.nextLine().charAt(0);
+			possibleAnswer = question.getPossibleAnswer(choice);
+			if (possibleAnswer == null) {
+				o.println("    Answer doesn't exit, try again.");
+			}
+		}
+		if (possibleAnswer.isCorrect()) {
+			this.scoreAmount++;
+		}
 	}
 	
 	public Quiz getQuiz() {
